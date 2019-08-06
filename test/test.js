a20a9ec7a44b1d231a7b67fa413fa3d3dbf3a18b
nsdq12345 <yeungjian@gmail.com>
2019-08-05 20:56:02 -0700

    random commit

--- test/test.js
+++ test/test.js
@@ -89,7 +89,7 @@ describe('todos', () => {
 
     it('should only save todo text contents in file', (done) => {
       const todoText = 'walk the dog';
-      todos.create(todoText, (err, todo) => {
+      todos.create(todoText, (err, {todo}) => {
         const todoFileContents = fs.readFileSync(path.join(todos.dataDir, `${todo.id}.txt`)).toString();
         expect(todoFileContents).to.equal(todoText);
         done();
